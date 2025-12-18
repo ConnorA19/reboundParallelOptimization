e3f9af8c0d789c34a7848a7805f8581a1f659fee
russell shaver <shave083@umn.edu>
2025-12-17 21:01:47 -0600

    done with DynamicDecomposition

--- examples/speedups_unittests/problem.c
+++ examples/speedups_unittests/problem.c
@@ -96,6 +96,77 @@ struct reb_simulation* mockShearingSheetSimulation(int collisionDetectionType, i
     return r;
 }
 
+struct reb_simulation* mockUnbalancedShearingSheetSimulation(int collisionDetectionType, int numParticles){
+    struct reb_simulation* r = reb_simulation_create();
+    // Setup constants
+    r->opening_angle2    = .5;                  // This determines the precission of the tree code gravity calculation.
+    r->integrator        = REB_INTEGRATOR_SEI;
+    r->boundary          = REB_BOUNDARY_SHEAR;
+    r->gravity           = REB_GRAVITY_TREE;
+    r->collision         = collisionDetectionType;
+    r->collision_resolve = reb_collision_resolve_hardsphere;
+    double OMEGA         = 0.00013143527;       // 1/s
+    r->ri_sei.OMEGA      = OMEGA;
+    r->G                 = 6.67428e-11;         // N / (1e-5 kg)^2 m^2
+    r->softening         = 0.1;                 // m
+    r->dt                = 1e-3*2.*M_PI/OMEGA;  // s
+    r->heartbeat         = heartbeat;           // function pointer for heartbeat
+    // This example uses two root boxes in the x and y direction.
+    // Although not necessary in this case, it allows for the parallelization using MPI.
+    // See Rein & Liu for a description of what a root box is in this context.
+    double surfacedensity          = 400;     // kg/m^2
+    double particle_density        = 400;     // kg/m^3
+    double particle_radius_min     = 1;       // m
+    double boxsize             = 100;         // m
+    reb_simulation_configure_box(r, boxsize, 2, 2, 1);
+    r->N_ghost_x = 2;
+    r->N_ghost_y = 2;
+    r->N_ghost_z = 0;
+
+    // Use Bridges et al coefficient of restitution.
+    r->coefficient_of_restitution = coefficient_of_restitution_bridges;
+    // When two particles collide and the relative velocity is zero, the might sink into each other in the next time step.
+    // By adding a small repulsive velocity to each collision, we prevent this from happening.
+    r->minimum_collision_velocity = particle_radius_min*OMEGA*0.001;  // small fraction of the shear accross a particle
+
+
+    // Add all ring paricles
+    double total_mass = surfacedensity*r->boxsize.x*r->boxsize.y;
+    double mass = 0;
+    int i = 0;
+    while(mass<total_mass && i < numParticles){
+        //Make all the particles the same size and touch eachother (size 2)
+        struct reb_particle pt;
+        if (i < numParticles/8) {
+            // spread 1/4th of the particles out along the box size
+            // pt.x = (r->boxsize.x - 2 * r->boxsize.x * (i & 1)) / (i/4 + 2);
+            // pt.y = (r->boxsize.y - 2 * r->boxsize.y * ((i & 2) >> 1)) / (i/4 + 2);
+            // pt.z = (r->boxsize.z - 2 * r->boxsize.z * ((i & 4) >> 2)) / (i/4 + 2);
+            pt.x = (r->boxsize.x/2) / (i+1) - 1;
+            pt.y = (r->boxsize.y/2) / (i+1) - 1;
+            pt.z = -(r->boxsize.z/2) / (i+1) + 1;
+        } else {
+            pt.x         = -r->boxsize.x/2. + i;
+            pt.y         = -r->boxsize.y/2. + i;
+            pt.z         = 0.5;                    // m
+        }
+        pt.vx         = 0;
+        pt.vy         = -1.5*pt.x*OMEGA;
+        pt.vz         = 0;
+        pt.ax         = 0;
+        pt.ay         = 0;
+        pt.az         = 0;
+        double radius     = 2.0;
+        pt.r         = radius;                        // m
+        double        particle_mass = particle_density*4./3.*M_PI*radius*radius*radius;
+        pt.m         = particle_mass;     // kg
+        reb_simulation_add(r, pt);
+        mass += particle_mass;
+        i+= 1;
+    }
+    return r;
+}
+
 struct reb_simulation* mockBouncingBallsSimulation(){
     struct reb_simulation* r = reb_simulation_create();
 
@@ -193,13 +264,13 @@ void time_CollisionSearchSpeedupCompare(){
     double total_time2 = 0.0;
     struct reb_simulation* r;
     for (int i = 0; i < 50000; i++){
-        r = mockShearingSheetSimulation(REB_COLLISION_TREE, i % 200);
+        r = mockUnbalancedShearingSheetSimulation(REB_COLLISION_TREE, i % 200);
         total_time1 += mock_reb_collision_search(r);
         reb_simulation_free(r);
     }
 
     for (int i = 0; i < 50000; i++){
-        r = mockShearingSheetSimulation(REB_COLLISION_TREE, i % 200);
+        r = mockUnbalancedShearingSheetSimulation(REB_COLLISION_TREE, i % 200);
         total_time2 += speedup_reb_collision_search(r);
         reb_simulation_free(r);
     }
