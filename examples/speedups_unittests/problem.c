df52059da57e21967a68c5aa0eb54edb2cf12535
ConnorA19 <connor.antony19@gmail.com>
2025-12-18 21:00:08 -0600

    added random tests

--- examples/speedups_unittests/problem.c
+++ examples/speedups_unittests/problem.c
@@ -128,6 +128,66 @@ struct reb_simulation* mockBouncingBallsSimulation(){
 }
 
 
+struct reb_simulation* mockRandomShearingSheetSimulation(int collisionDetectionType, int numParticles, struct reb_particle* particleList){
+    particleList = malloc(sizeof(struct reb_particle) * numParticles);
+    struct reb_simulation* r = reb_simulation_create();
+    // Setup constants
+    r->opening_angle2    = .5;                  // This determines the precission of the tree code gravity calculation.
+    r->integrator        = REB_INTEGRATOR_SEI;
+    r->boundary          = REB_BOUNDARY_SHEAR;
+    r->gravity           = REB_GRAVITY_TREE;
+    r->collision         = REB_COLLISION_TREE;
+    r->collision_resolve = reb_collision_resolve_hardsphere;
+    double OMEGA         = 0.00013143527;       // 1/s
+    r->ri_sei.OMEGA      = OMEGA;
+    r->G                 = 6.67428e-11;         // N / (1e-5 kg)^2 m^2
+    r->softening         = 0.1;                 // m
+    r->dt                = 1e-3*2.*M_PI/OMEGA;  // s
+    r->heartbeat         = heartbeat;           // function pointer for heartbeat
+    // This example uses two root boxes in the x and y direction.
+    // Although not necessary in this case, it allows for the parallelization using MPI.
+    // See Rein & Liu for a description of what a root box is in this context.
+    double surfacedensity          = 400;     // kg/m^2
+    double particle_density        = 400;     // kg/m^3
+    double particle_radius_min     = 1;       // m
+    double particle_radius_max     = 4;       // m
+    double particle_radius_slope   = -3;
+    double boxsize             = 100;         // m
+    reb_simulation_configure_box(r, boxsize, 2, 2, 1);
+    r->N_ghost_x = 2;
+    r->N_ghost_y = 2;
+    r->N_ghost_z = 0;
+
+    // Initial conditio
+    // Use Bridges et al coefficient of restitution.
+    r->coefficient_of_restitution = coefficient_of_restitution_bridges;
+    // When two particles collide and the relative velocity is zero, the might sink into each other in the next time step.
+    // By adding a small repulsive velocity to each collision, we prevent this from happening.
+    r->minimum_collision_velocity = particle_radius_min*OMEGA*0.001;  // small fraction of the shear accross a particle
+
+
+    // Add all ring paricles
+    for(int i =0; i < numParticles; i++){
+        struct reb_particle pt;
+        pt.x         = reb_random_uniform(r, -r->boxsize.x/2.,r->boxsize.x/2.);
+        pt.y         = reb_random_uniform(r, -r->boxsize.y/2.,r->boxsize.y/2.);
+        pt.z         = reb_random_normal(r, 1.);                    // m
+        pt.vx         = 0;
+        pt.vy         = -1.5*pt.x*OMEGA;
+        pt.vz         = 0;
+        pt.ax         = 0;
+        pt.ay         = 0;
+        pt.az         = 0;
+        double radius     = reb_random_powerlaw(r, particle_radius_min,particle_radius_max,particle_radius_slope);
+        pt.r         = radius;                        // m
+        double        particle_mass = particle_density*4./3.*M_PI*radius*radius*radius;
+        pt.m         = particle_mass;     // kg
+        reb_simulation_add(r, pt);
+        particleList[i] = pt;
+    }
+    return r;
+}
+
 //Tests  ---------------------------------
 
 
@@ -157,6 +217,7 @@ void test_CollisionSearchCompare(int particleCount){
     assert(collisions_original == collisions_new);
     reb_simulation_free(r);
 }
+
 
 
 void time_CollisionSearchOriginal(){
@@ -216,9 +277,13 @@ int main(int argc, char* argv[]){
     test_CollisionSearchCompare(2);
     test_CollisionSearchCompare(10);
     test_CollisionSearchCompare(100);
+
     // test_CollisionSearchCompare(1000);
     //WANT TO ADD END TO END TEST HERE WITH OUTPUT TO BIN
 
+
+    struct reb_particle* particleList;
+    struct reb_simulation* r = mockRandomShearingSheetSimulation(REB_COLLISION_TREE, 10, particleList);
     //TIME
     // time_CollisionSearchSpeedup();
     time_CollisionSearchSpeedupCompare();
@@ -230,5 +295,5 @@ int main(int argc, char* argv[]){
     // time_FullCollisionSearchOriginal(r);
     //time_CollisionSearchOriginal(r);
     // reb_simulation_free(r);
-
+    free(particleList);
 }
